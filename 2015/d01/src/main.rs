e672a008f785df8810c7a0d209ae76dbdd72b6b7
V-Karch <luna.karch05@gmail.com>
2024-10-20 22:08:50 -0400

    2015/d01: added correct solution for part 1

--- 2015/d01/src/main.rs
+++ 2015/d01/src/main.rs
@@ -2,5 +2,20 @@ fn main() {
     let contents: String = std::fs::read_to_string("input.txt")
         .expect("File `input.txt` could not be read. Is it in the correct location?");
 
-    println!("{}", contents);
+    let part1_result: i32 = part1(&contents);
+    println!("Result of part 1: {}", part1_result);
+}
+
+fn part1(contents: &String) -> i32 {
+    let mut result: i32 = 0;
+
+    for i in contents.chars() {
+        if i == '(' {
+            result += 1;
+        } else {
+            result -= 1;
+        }
+    }
+
+    return result;
 }
