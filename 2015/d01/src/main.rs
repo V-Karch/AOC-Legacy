21f7f1491750b8e4c1b84d1636b731198cbbbc92
V-Karch <luna.karch05@gmail.com>
2024-10-20 21:59:30 -0400

    2015/d01: simplified program to read from file

--- 2015/d01/src/main.rs
+++ 2015/d01/src/main.rs
@@ -1,14 +1,6 @@
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
-    if args.len()  != 2 {
-        println!("Error: Incorrect usage");
-        println!("Expected 1 argument but got {}", args.len() - 1);
-        return;
-    }
-
-    let contents: String = std::fs::read_to_string(&args[1])
-        .expect("File could not be read");
+    let contents: String = std::fs::read_to_string("input.txt")
+        .expect("File `input.txt` could not be read. Is it in the correct location?");
 
     println!("{}", contents);
 }
