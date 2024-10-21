36d594706bedbe99153625035a72c7b1b157c1af
V-Karch <luna.karch05@gmail.com>
2024-10-21 12:42:39 -0400

    2015/d05: added day 5 part 2 solution

--- 2015/d05/src/main.rs
+++ 2015/d05/src/main.rs
@@ -4,13 +4,16 @@ fn main() {
 
     let part1_result = part1(&contents);
     println!("Part 1: {}", part1_result);
+
+    let part2_result = part2(&contents);
+    println!("Part 2: {}", part2_result);
 }
 
 fn part1(contents: &String) -> u32 {
     let mut count: u32 = 0;
 
     for line in contents.split("\n") {
-        if is_nice(line) {
+        if is_nice_part1(line) {
             count += 1
         }
     }
@@ -18,7 +21,50 @@ fn part1(contents: &String) -> u32 {
     return count;
 }
 
-fn is_nice(line: &str) -> bool {
+fn part2(contents: &String) -> u32 {
+    let mut count: u32 = 0;
+
+    for line in contents.split("\n") {
+        if is_nice_part2(line) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+fn is_nice_part2(line: &str) -> bool {
+    return check_letter_gap(line) && has_non_overlapping_pair(line);
+}
+
+fn has_non_overlapping_pair(line: &str) -> bool {
+    let len = line.len();
+
+    for i in 0..len - 1 {
+        let pair = &line[i..i + 2];
+
+        match line[i + 2..].find(pair) {
+            Some(_) => return true,
+            None => continue,
+        }
+    }
+
+    return false;
+}
+
+fn check_letter_gap(line: &str) -> bool {
+    let chars: Vec<char> = line.chars().collect();
+
+    for i in 2..chars.len() {
+        if chars[i] == chars[i - 2] {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn is_nice_part1(line: &str) -> bool {
     return vowel_count(line) >= 3 && check_double_char(line) && !check_specific_strings(line);
 }
 
