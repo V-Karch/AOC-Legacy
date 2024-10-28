c35fee3408b8fd27fbc2efac65b53cf03071f649
V-Karch <luna.karch05@gmail.com>
2024-10-28 16:12:39 -0400

    2015/d08: added day 8 part 2 solution

--- 2015/d08/src/main.rs
+++ 2015/d08/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let contents: String =
-        std::fs::read_to_string("input.txt").expect("Unable to read file.");
+    let contents: String = std::fs::read_to_string("input.txt").expect("Unable to read file.");
 
     let part1_result: usize = part1(&contents);
     println!("Part 1: {}", part1_result);
+
+    let part2_result: usize = part2(&contents);
+    println!("Part 2: {}", part2_result);
 }
 
 fn part1(contents: &String) -> usize {
@@ -19,7 +21,22 @@ fn part1(contents: &String) -> usize {
     }
 
     let difference: usize = total_code_characters - total_memory_characters;
+    return difference;
+}
 
+fn part2(contents: &String) -> usize {
+    let mut total_code_characters: usize = 0;
+    let mut total_encoded_characters: usize = 0;
+
+    for line in contents.lines() {
+        let code_length: usize = line.len();
+        let encoded_length: usize = eval_encoded_string_length(line);
+
+        total_code_characters += code_length;
+        total_encoded_characters += encoded_length;
+    }
+
+    let difference: usize = total_encoded_characters - total_code_characters;
     return difference;
 }
 
@@ -58,4 +75,18 @@ fn eval_string_memory_length(s: &str) -> usize {
     }
 
     return memory_length;
+}
+
+fn eval_encoded_string_length(s: &str) -> usize {
+    let mut encoded_length: usize = 2; // Account for the surrounding quotes
+
+    for c in s.chars() {
+        match c {
+            '"' => encoded_length += 2,  // " becomes \"
+            '\\' => encoded_length += 2, // \ becomes \\
+            _ => encoded_length += 1,    // Other characters remain the same
+        }
+    }
+
+    return encoded_length;
 }
