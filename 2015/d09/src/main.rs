7c9ccab55b8efa98b797a82657d501b4faec0e08
V-Karch <luna.karch05@gmail.com>
2024-10-28 16:27:12 -0400

    2015/d09: added an Edge struct to start constructing a graph

--- 2015/d09/src/main.rs
+++ 2015/d09/src/main.rs
@@ -1,8 +1,27 @@
+#[derive(Debug)]
+struct Edge {
+    base: String,
+    goal: String,
+    distance: u32
+}
+
 fn main() {
     let contents = std::fs::read_to_string("input.txt")
         .expect("Unable to read file.");
 
     for line in contents.lines() {
-        println!("{}", line);
+        let pieces: Vec<&str> = line.split(" ").collect();
+
+        let base: &str = pieces[0];
+        let goal: &str = pieces[2];
+        let distance: u32 = pieces[4].parse::<u32>().expect("Failed to parse int");
+
+        let edge = Edge {
+            base: (base).to_string(),
+            goal: (goal).to_string(),
+            distance
+        };
+        
+        println!("{:?}", edge);
     }
 }
