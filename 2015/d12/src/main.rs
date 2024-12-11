6c4f1191142429cb443317ccc68fe9db402b02d9
V-Karch <luna.karch05@gmail.com>
2024-12-11 01:42:58 -0500

    2015/d12: implemented part 1 solution

--- 2015/d12/src/main.rs
+++ 2015/d12/src/main.rs
@@ -1,3 +1,54 @@
 fn main() {
-    println!("Hello, world!");
+    let contents = std::fs::read_to_string("input.json").expect("Failed to read input file");
+    let part1_result = part1(contents);
+
+    println!("Part 1: {}", part1_result);
+}
+
+fn part1(contents: String) -> i32 {
+    let mut in_number = false;
+    let mut temp_num: Vec<char> = Vec::new();
+    let mut sum = 0;
+
+    for c in contents.chars() {
+        if is_number_part(c) {
+            in_number = true;
+        }
+
+        if in_number && is_number_part(c) {
+            temp_num.push(c);
+        }
+
+        if in_number && is_number_part(c) == false {
+            in_number = false;
+
+            let value = temp_num
+                .iter()
+                .collect::<String>()
+                .parse::<i32>()
+                .expect("Failed to parse Vec");
+
+            sum += value;
+            temp_num.clear();
+        }
+    }
+
+    return sum;
+}
+
+fn is_number_part(c: char) -> bool {
+    return match c {
+        '-' => true,
+        '0' => true,
+        '1' => true,
+        '2' => true,
+        '3' => true,
+        '4' => true,
+        '5' => true,
+        '6' => true,
+        '7' => true,
+        '8' => true,
+        '9' => true,
+        _ => false,
+    };
 }
